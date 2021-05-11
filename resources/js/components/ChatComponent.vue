e9dcab76479989c83ca0bf51674b111ff844cde2
Francisco Jesus <franj.adan@gmail.com>
2021-05-11 13:52:04 +0200

    Mantener color en los mensajes nuevos

--- resources/js/components/ChatComponent.vue
+++ resources/js/components/ChatComponent.vue
@@ -1,9 +1,15 @@
+<style scoped>
+  .bg-green{
+      background-color: #c7e5b3;
+  }
+</style>
+
 <template>
     <div class="row justify-content-center">
         <div class="col-md-12">
             <h2>Mensajes</h2>
             <div id="messages_div" class="card mx-2 my-2 bg-light overflow-auto" style="max-height: 600px; min-height: 50px;" >
-                <div :style="[message.user_id == userId ? {'background-color': '#c7e5b3'} : '']" :class="{'card mx-2 my-2 col-md-5 p-2 message rounded align-self-end ': message.user.id == userId, 'card mx-2 my-2 col-md-5 p-2 message rounded': message.user.id != userId }" v-for="message in messages" :key="message.id">
+                <div :class="{'card mx-2 my-2 col-md-5 p-2 message rounded align-self-end bg-green': message.user.id == userId, 'card mx-2 my-2 col-md-5 p-2 message rounded': message.user.id != userId }" v-for="message in messages" :key="message.id">
                     <div v-if="message.user.id == userId">
                         <p style="margin: 0px;">{{ message.message }}</p>
                         <p style="font-size: 10px; margin: 0px;" class="text-right">{{ message.created_at | moment }}</p>
